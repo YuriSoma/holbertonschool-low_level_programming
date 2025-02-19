696076ed5dc2badc1177505357fb3fa0e46e36cc
YuriSoma <yaser.alelayan@outlook.com>
2025-02-19 16:49:55 +0000

    (C - Variables, if, else, while) project pusher

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
