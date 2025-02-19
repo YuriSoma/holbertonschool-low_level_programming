3bc0b4d8f7b0c811148c713f326d6fecdc46965c
YuriSoma <yaser.alelayan@outlook.com>
2025-02-19 15:02:24 +0000

    (C - Variables, if, else, while) project pusher

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,14 +11,22 @@
 int main(void)
 {
 	int n;
+	int digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	digit = n;
+	while (digit >= 10){
+		digit %= 10;
+	}
+	if (digit > 5){
+		printf("Last digit of %d is %d and is greater than 5", n, digit);
+	}
+	if (digit == 0){
+		printf("Last digit of %d is %d and is 0", n, digit);
+	}
+	if (digit < 6 && != 0){
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, digit);
+	}
 	return (0);
 }
