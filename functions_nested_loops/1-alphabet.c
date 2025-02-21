8924282996e2e53a4a3223e5b0567a976a756b0c
YuriSoma <yaser.alelayan@outlook.com>
2025-02-21 14:29:01 +0000

    (C - Variables, if, else, while) project pusher

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet - print all alphabets
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
