e2b04018bf2ba43b30ffe02002b40d27b75f6736
YuriSoma <yaser.alelayan@outlook.com>
2025-02-21 19:21:03 +0000

    (C - Variables, if, else, while) project pusher

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+ * _isalpha - checks if the character is an alpha
+ * @c: The character to be check
+ *
+ * Return: On success 1.
+ * On error, 0 is returned.
+ */
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+		return (1);
+	return (0);
+}
