6f4a6597be5339e247bf7bea6a9f322062df0467
YuriSoma <yaser.alelayan@outlook.com>
2025-02-22 00:31:08 +0000

    (C - Variables, if, else, while) project pusher

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * print_last_digit - computes the absoulte value of an int.
+ * @c: The character to be check
+ *
+ * Return: Abs value
+ */
+void jack_bauer(void)
+{
+	int i, x;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (x = 0; x <= 59; x++)
+		{
+			if (i <= 9)
+			{
+				if (x <= 9)
+				{
+					_putchar('0');
+					_putchar(i);
+					_putchar(':');
+					_putchar('0');
+                                        _putchar(x);
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar('0');
+					_putchar(i);
+					_putchar(':');
+					_putchar(x);
+					_putchar('\n');
+				}
+			}
+			else
+			{
+				if (x <= 9)
+				{
+					_putchar(i);
+					_putchar(':');
+					_putchar('0');
+					_putchar(x);
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(i);
+					_putchar(':');
+					_putchar(x);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
