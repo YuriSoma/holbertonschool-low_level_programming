bd5bbeb301d3c1b2d4ed0639c5be201163b8431a
YuriSoma <yaser.alelayan@outlook.com>
2025-02-22 01:29:16 +0000

    (C - Variables, if, else, while) project pusher

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,15 +6,34 @@
  */
 void times_table(void)
 {
-	int x, y;
+	int i, j, n;
 
-	for (x = 0; x < 10; x++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (y = 0; y < 10; y++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar((x * y) + '0');
-			_putchar(',');
-			_putchar(' ');
+			n = i * j;
+
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
