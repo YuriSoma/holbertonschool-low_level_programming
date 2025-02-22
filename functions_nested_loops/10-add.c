85d5ed4848cd7cdd889e490a62e585e62c65ec3c
YuriSoma <yaser.alelayan@outlook.com>
2025-02-22 01:35:58 +0000

    (C - Variables, if, else, while) project pusher

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - prints tale 9
+ * @a: param for first arg
+ * @b: param for second arg
+ * Description: Function that adds two integers and returns the result
+ * Return: returns the addition of the two args
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
