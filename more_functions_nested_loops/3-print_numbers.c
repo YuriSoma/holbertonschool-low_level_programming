56727530d3d7b651d50dfd51bc97b6feb111de7b
YuriSoma <yaser.alelayan@outlook.com>
2025-02-24 08:55:17 +0000

    (C - Variables, if, else, while) project pusher

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9 followed by new line.
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a ++)
+	{
+		_putchar(a);
+	}
+}
