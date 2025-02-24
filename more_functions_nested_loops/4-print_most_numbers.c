85a7802b2948e1b42cc6e75e5794a9953f957a04
YuriSoma <yaser.alelayan@outlook.com>
2025-02-24 09:02:55 +0000

    (C - Variables, if, else, while) project pusher

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 - 9 (not 2 or 4) followed by new line.
+ *
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a == 50 || a == 52)
+			continue;
+		_putchar(a);
+	}
+	_putchar('\n');
+}
