da6c62f10354c5e8262f803ae049cd2f93f9a82d
YuriSoma <yaser.alelayan@outlook.com>
2025-02-24 08:45:33 +0000

    (C - Variables, if, else, while) project pusher

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -  multiply 2 digits.
+ * @a: first digit.
+ * @b: second digit.
+ *
+ * Return: the result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	int r = a * b;
+	return (r);
+}
