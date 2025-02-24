b86fe1c5c87ea173fef119278439c430746667c3
YuriSoma <yaser.alelayan@outlook.com>
2025-02-24 18:47:33 +0000

    (C - Variables, if, else, while) project pusher

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints square with space in new line n times.
+ * @n: the number of printing times.
+ *
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
