e019a49d2247693c0796a124176b12999bb1e134
YuriSoma <yaser.alelayan@outlook.com>
2025-02-26 08:06:32 +0000

    (C - Variables, if, else, while) project pusher

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle as passing size.
+ * @size: the size of the triangle.
+ *
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+	for (a = 0; a < size; a++)
+	{
+		b = 0;
+		while (b < size)
+		{
+			if (b >= a)
+				_putchar('#');
+			else
+				_putchar(' ');
+			b++;
+		}
+		_putchar('\n');
+	}
+	}
+	else
+		_putchar('\n');
+}
