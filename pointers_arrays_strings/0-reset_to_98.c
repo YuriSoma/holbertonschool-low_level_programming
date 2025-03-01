2e9b919e22e8f195e736f2a5b8069e97428c6c01
YuriSoma <yaser.alelayan@outlook.com>
2025-03-01 18:26:57 +0000

    project 'C - Pointers, arrays and strings' pusher

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - It reset the n valu to be 98 using pointer dereferencing.
+ * @n: pointer to n.
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains all tasks of Holberton School project "C - Pointers, arrays and strings"
