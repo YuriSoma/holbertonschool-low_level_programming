c888f3bac6256097c1fe22b7a55e4d7d2e13df69
YuriSoma <yaser.alelayan@outlook.com>
2025-03-06 23:08:14 +0000

    project 'C - Pointers, arrays and strings' pusher

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i <= '\0'; i++)
+	for (i = 0; i <= '\0'; s++)
 	{
 		if (s[i] == c)
 		{
-			return (s+i);
+			return (s);
 		}
+		i++;
 
 	}
 }
