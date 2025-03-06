1fef425e390314718d446b5bb245408bead83ed5
YuriSoma <yaser.alelayan@outlook.com>
2025-03-06 17:18:02 +0000

    project 'C - Pointers, arrays and strings' pusher

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - sets to buffer a constant byte b
+ * @s: char array contans the buffer
+ * @b: the constant byte to be sets
+ * @n: the limits b will be filled
+ *
+ * Return: the array s after setting
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for(i = 0; i < n; i ++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
