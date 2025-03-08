11ff102c68941c60882f06947b8086ce15f4e8ec
YuriSoma <yaser.alelayan@outlook.com>
2025-03-08 20:20:58 +0000

    project 'C - Recursion' pusher

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string using recursion in reverse
+ * @s: address of string to be printed reversely
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s[0] != '0')
+		_print_rev_recursion(s++);
+	_putchar(s[0]);
+}
