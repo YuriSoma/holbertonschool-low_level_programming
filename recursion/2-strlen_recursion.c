db633ba9eba75fce0d262f8b5d96e5df1801a08b
YuriSoma <yaser.alelayan@outlook.com>
2025-03-08 21:20:35 +0000

    project 'C - Recursion' pusher

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,14 +9,6 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (s[0] != '\0')
-	{
-		len++;
-		_strlen_recursion(s + 1);
-	}
-	else
-	{
-		return (len);
-	}
+	return (_strlen_recursion() + len);
 
 }
