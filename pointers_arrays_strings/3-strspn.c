aed975840f3ec5a2817d4459058146b7ac3cff11
YuriSoma <yaser.alelayan@outlook.com>
2025-03-08 03:16:37 +0000

    project 'C - Pointers, arrays and strings' pusher

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strspn - 
+ * @s: the string
+ * @c: letter to be located
+ *
+ * Return: s after locating c.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != c; s++)
+	{
+		if (s[i] == '\0')
+			return (0);
+	}
+	return (s);
+}
