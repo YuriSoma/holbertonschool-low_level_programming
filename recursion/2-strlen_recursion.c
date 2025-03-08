3522dd66bb0500485084923d6a1ae0bb50c759c5
YuriSoma <yaser.alelayan@outlook.com>
2025-03-08 21:13:55 +0000

    project 'C - Recursion' pusher

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts a string len
+ * @s: string to count its len
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (s[0] != '\0')
+	{
+		len++;
+		_print_rev_recursion(s + 1);
+	}
+	else
+	{
+		return (len);
+	}
+
+}
