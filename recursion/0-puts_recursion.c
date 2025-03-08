82a842ae0aa3378e413616bd14d620014e1040f1
YuriSoma <yaser.alelayan@outlook.com>
2025-03-08 19:37:57 +0000

    project 'C - Pointers, arrays and strings' pusher

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,23 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_diagsums - calculate the sum of the two diagonals
- * @a: 2d array address
- * @size: the 2d array square size
+ * _puts_recursion - prints string using recursion
+ * @s: address of string to be printed
  *
  */
-void print_diagsums(int *a, int size)
+void _puts_recursion(char *s)
 {
-	int i, x, sum1 = 0, sum2 = 0;
+	int i;
 
-	for (i = 0; i < size * size; i += size + 1)
-	{
-		sum1 += a[i];
-	}
-	for (x = size - 1; x < size * size - 1; x += size - 1)
-	{
-		sum2 += a[x];
-	}
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; s[i] < '\0'; i++)
+		_putchar(s[i]);
 }
