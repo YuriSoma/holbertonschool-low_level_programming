f9b9273042ea6378e2dc76e92c69e3be792af2e4
root <yaser.alelayan@outlook.com>
2025-03-09 13:53:28 +0000

    project 'C - Recursion' pusher

---
+++ recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * factorial - returna factorial of a givin number
+ * @n: number to counts its factorial
+ *
+ * Return: the result of factorial n or -1 if n equals -1
+ */
+int factorial(int n)
+{
+	
+}
