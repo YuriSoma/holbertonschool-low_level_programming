2a7246632af557f3e415729366d287973ae91b09
YuriSoma <yaser.alelayan@outlook.com>
2025-03-10 22:10:29 +0000

    project 'C - Recursion' pusher

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds the square root of a given number.
+ * @n: the given number to finds its square root.
+ *
+ * Return: square root of n or -1 if n has no root.
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+	float rtroot(float x, float y)
+	{
+		if (x / y == y)
+		{
+			return (y);
+		}
+		elses if (y >= x)
+		{
+			return (-1);
+		}
+		rtroot(x, y + 1.0);
+	}
+
+	i = return rtroot(n, 1.0);
+	return (i);
+}
