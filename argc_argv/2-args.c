5066bd15ac11131604e3304fb032456ff0b4fd19
YuriSoma <yaser.alelayan@outlook.com>
2025-03-13 23:52:21 +0000

    project 'C - argc, argv' pusher

---
+++ argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point and prints every argument in new line ends with new line.
+ * @argc: arguments counter
+ * @argv: pointer of the argument vector which is an array of all arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc__attribute__((unused)), char *argv[])
+{
+	int i = 0, x;
+
+	while (argv[i])
+	{
+		for (x = 0; argv[i][x] != '\0'; x++)
+		{
+			_putchar(argv[i][x]);
+		}
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
