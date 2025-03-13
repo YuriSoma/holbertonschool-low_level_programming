85b441ac3a94001f5acc4de41b11f69ea80532b1
YuriSoma <yaser.alelayan@outlook.com>
2025-03-13 23:28:44 +0000

    project 'C - argc, argv' pusher

---
+++ argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point and printing the program arguments counter.
+ * @argc: arguments counter
+ * @argv: pointer of the argument vector which is an array of all arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[i])
+	{
+		i++;
+	}
+	if (argc <= 9)
+		_putchar(argc + '0');
+	_putchar('\n');
+	return (0);
+}
