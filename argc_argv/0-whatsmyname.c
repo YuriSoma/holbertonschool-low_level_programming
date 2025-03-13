678cf925d72831416e7f8862d8f38e0d303e5789
YuriSoma <yaser.alelayan@outlook.com>
2025-03-13 22:22:23 +0000

    project 'C - argc, argv' pusher

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,10 +7,10 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	
-	for (i = 0; i < argv[argc]; i++)
+
+	for (i = 0; i < argv[argc - 1]; i++)
 	{
 		_putchar(*argv[0][i]);
 	}
-	return (0);
+        return (0);
 }
