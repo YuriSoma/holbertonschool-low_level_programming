926e82b986bff876263e8959dffdb8f11114e120
YuriSoma <yaser.alelayan@outlook.com>
2025-03-14 00:04:23 +0000

    project 'C - argc, argv' pusher

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point and does multiplication of 2 arguments, and prints Error
+ * if no enough number arguments.
+ * @argc: arguments counter (Unused)
+ * @argv: pointer of the argument vector which is an array of all arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, x = 2;
+
+	(void)argc;
+	if(i && x)
+	{
+		printf("%d\n", i * x);
+	}
+	else
+	{
+		printf("Error");
+	}
+	return (0);
+}
