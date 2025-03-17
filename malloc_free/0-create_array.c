789505d7da08388adfd1c578a2125b8f31409140
YuriSoma <yaser.alelayan@outlook.com>
2025-03-17 22:28:22 +0000

    project 'C - malloc, free' pusher

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * create_array - Function to create an array initilized with c using malloc.
+ * @size: size of the array
+ * @c: character that the array initilized with.
+ *
+ * Return: 0 on failure or if size equals 0 otherwise returns the pointer.
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (0);
+	char *init_arr = (char *) malloc(size * sizeof(char));
+	init_arr = c;
+	if (!init_arr)
+		return (0);
+	return (init_arr);
+}
