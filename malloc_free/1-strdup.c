08699b3bf6474c8ced76e172a6e1fe476e6763d3
YuriSoma <yaser.alelayan@outlook.com>
2025-03-19 14:22:22 +0000

    project 'C - malloc, free' pusher

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Function to create an array initilized with c using malloc.
+ * @size: size of the array
+ * @c: character that the array initilized with.
+ *
+ * Return: 0 on failure or if size equals 0 otherwise returns the pointer.
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+
+	}
+}
