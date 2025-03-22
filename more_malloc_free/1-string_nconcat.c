ba171ba05d7fb312d6fca7492c1ee9ce00e77352
YuriSoma <yaser.alelayan@outlook.com>
2025-03-22 04:28:51 +0000

    project 'C - More malloc, free' pusher

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (conc_n == NULL)
 		return (NULL);
 	len = 0;
-	for (i = 0; s1[i]; index++)
+	for (i = 0; s1[i]; i++)
 		conc_n[len++] = s1[i];
 	for (i = 0; s2[i] && i < n; i++)
 		conc_n[len++] = s2[i];
