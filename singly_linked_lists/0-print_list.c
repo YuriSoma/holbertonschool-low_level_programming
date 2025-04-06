e5409467000b00c77b5c767027cc9db2c3137235
YuriSoma <yaser.alelayan@outlook.com>
2025-04-06 13:16:58 +0000

    First push

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,6 +3,21 @@
 #include "lists.h"
 
 /**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/**
  * print_list - prints all the linked list items.
  * @h: struct linked list which the function will print its items.
  *
