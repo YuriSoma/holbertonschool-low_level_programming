ace6fce5c468b930932e2d5228af541d311a56bf
YuriSoma <yaser.alelayan@outlook.com>
2025-04-07 06:13:08 +0000

    Create 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - free a giving list.
+ * @head: pointer of the list to be freed.
+ *
+ */
+
+void free_list(list_t *head)
+{
+	free(head);
+}
