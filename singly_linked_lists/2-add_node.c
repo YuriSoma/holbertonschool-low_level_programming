205d2918adbdf3387b2fff10d50dd37d891fbbf5
YuriSoma <yaser.alelayan@outlook.com>
2025-04-07 00:35:09 +0000

    Creating 2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds new element for a giving list.
+ * @head: pointer of a pointer to a list. 
+ * @str: a string to be duplicated to the new node.
+ *
+ * Return: the address of the new created node.
+ *
+ * Description: the linked list structure will have 3 items:
+ * 1- str: a string 2- len: length of the string 3- next: pointer of next node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	new->str = strdup(str);
+	head->next = new;
+
+	return (head->next);
+}
