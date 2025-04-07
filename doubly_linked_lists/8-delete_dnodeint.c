03672eb4c60503437c00ab3419bbdb7bc3672e23
YuriSoma <yaser.alelayan@outlook.com>
2025-04-07 14:19:09 +0000

    Initialize Task-8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete a node by its index.
+ * @head: a given doubly linked list.
+ * @index: a given index.
+ *
+ * Return: 1 on Success, or -1 on failure.
+ *
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (!head)
+		return (-1);
+	while (index != 0)
+	{
+		if (!temp)
+			return (-1);
+		temp = temp->next;
+		index--;
+	}
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (*head)
+			(*head)->prev = 0;
+	}
+
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != 0)
+			temp->next->prev = temp->prev;
+	}
+
+	free(temp);
+}
