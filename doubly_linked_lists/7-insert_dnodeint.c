001dac80ae9aad9d3e5f53def904e55f42fc5434
YuriSoma <yaser.alelayan@outlook.com>
2025-04-07 14:07:12 +0000

    Initialize Task-7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *new;
-	
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	while (idx != 1)
