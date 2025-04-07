ac455a643b1ec049b16b82ebf6a6ae14a22f248f
YuriSoma <yaser.alelayan@outlook.com>
2025-04-07 12:12:15 +0000

    Initialize Task-2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (0);
 	new->n = n;
+	new->prev = NULL;
 	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
+
 	return (new);
 }
